5c6076f79d0fb77f69be38eda559f2a3ab7556ff
Avinash kumar pandey <105911954+avinash-kr-pandey@users.noreply.github.com>
2024-09-23 18:09:15 +0530

    new commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -128,6 +128,17 @@ const Sidebar = () => {
                 </span>
               </Link>
             </li>
+            <li>
+              <Link
+                to="/feedback"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <FaPlusCircle className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                Students Feedback
+                </span>
+              </Link>
+            </li>
           </ul>
           <div className="relative h-[53%]">
             <div className="absolute bottom-0 flex flex-row justify-center items-end gap-3 p-4">
