40e2c9af530619973e3966346770fce3c45c1b82
Avinash kumar pandey <105911954+avinash-kr-pandey@users.noreply.github.com>
2024-09-25 18:29:37 +0530

    new commit

--- src/components/pages/testReport/AllStudentDetails.jsx
+++ src/components/pages/testReport/AllStudentDetails.jsx
@@ -302,7 +302,7 @@ fetchData();
                 <td className="border px-4 py-2">{getStatus(student)}</td>
                 <td className="border px-4 py-2">
                   <button
-                    onClick={() => handleView(student._id)}
+                    onClick={() => handleView(student.email)}
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                   >
                     View
